545262b7e2a48c4d74ca78845811a583ac4dc9e9
fkhd00 <aniketgdm@gmail.com>
2021-04-16 19:37:42 +0530

    Two players can play

--- tictactoe.js
+++ tictactoe.js
@@ -1,12 +1,20 @@
 let player1Value=0;
 let player2Value=0;
+let cpuValue=0;
+let player1Name="";
+let player2Name="";
+let wincount=0;
 function displayPlayers(){
     const button1=document.getElementById("player1-button");
     button1.addEventListener("click",()=>{
-        const player1=document.getElementById("player1Input").value;
+        let player1=document.getElementById("player1Input").value;
+        if(player1==""){
+            player1=player1+"player 1";
+        }
+        player1Name=player1;
         document.getElementById("player1Form").style.cssText="visibility:hidden;";
         document.getElementById("player1").innerHTML=""+player1;
-        document.getElementById("player1").style.cssText="font-size:40px; text-align:center;"
+        document.getElementById("player1").style.cssText="font-size:40px; text-align:center;";
     });
     const button2=document.getElementById("player2-button");
     let player2div=document.getElementById("player2");
@@ -28,8 +36,14 @@ function displayPlayers(){
         confirmbutton2.addEventListener("click",()=>{
             confirmbutton2.style.cssText="visibility:hidden;";
             player2Input.style.cssText="visibility:hidden;";
+            if (player2Input.value==""){
+                player2Input.value="player 2";
+                toss();
+            }
             player2div.innerHTML=player2Input.value+"";
+            player2Name=player2Input.value;
             player2div.style.cssText="font-size:40px;";
+            cpuValue=2;
             toss();
         });
     });
@@ -38,7 +52,9 @@ function displayPlayers(){
         button2.style.cssText="visibility:hidden";
         button3.style.cssText="visibility:hidden";
         player2div.innerHTML="CPU";
+        player2Name="CPU"
         player2div.style.cssText="font-size:40px;";
+        player2Value=2;
         toss();
     });
     
@@ -52,11 +68,21 @@ function displayPlayers(){
         tossButton.style.cssText="text-align:center; margin-top:30px;";
         tossButton.addEventListener("click",()=>{
             const toss=Math.floor(Math.random()*2);
-            if(toss==0){
+            if((toss==0&&cpuValue==2)||(toss==0&&player2Value==2)){
                 player1Value=1;
+                tossButton.style.cssText="visibility:hidden;";
+                document.getElementById("message").innerHTML="<b>"+player1Name+"</b><br> goes first";
+                console.log("p1 wins");
+            }
+            else if(toss==1&&cpuValue==2){
+                player2Value=1;
+                tossButton.style.cssText="visibility:hidden;";
+                document.getElementById("message").innerHTML="<b>"+player2Name+"</b><br> goes first";
             }
             else{
-                player2Value=1;
+                cpuValue==1;
+                tossButton.style.cssText="visibility:hidden;";
+                document.getElementById("message").innerHTML="<b>CPU</b><br> goes first";
             }
         });
     }
@@ -64,15 +90,67 @@ function displayPlayers(){
 }
 
 displayPlayers();
-let gridbox=[];
-let creategrid=function(){
-    for(i=0;i<9;i++){
-        gridbox[i]=document.createElement("div");
-        gridbox[i].id="id"+i;
-        gridbox[i].classList.add("boxes");
-        document.getElementById("grid").appendChild(gridbox[i]);
-        
 
+const game=()=>{
+
+    let gridbox={};
+    let turncount=0;
+    let _ip=["","","","","","","","",""];
+    let creategrid=function(){
+        for(let k=0;k<9;k++){
+            gridbox[k]=document.createElement("div");
+            gridbox[k].id=""+k;
+            gridbox[k].classList.add("boxes");
+            gridbox[k].count=0;
+            document.getElementById("grid").appendChild(gridbox[k]);
         }
+        for(let i=0;i<9;i++){
+            gridbox[i].addEventListener("click",()=>{ 
+                    if((turncount%2==0&&gridbox[i].count==0)&&(cpuValue==2||player2Value==2)){ 
+                        gridbox[i].innerHTML="O";
+                        _ip[gridbox[i].id]=_ip[gridbox[i].id]+"O";
+                        turncount=turncount+1;
+                        gridbox[i].count=1;
+                        
+                        if((_ip[0]+_ip[1]+_ip[2]=="OOO")||(_ip[3]+_ip[4]+_ip[5]=="OOO")||(_ip[6]+_ip[7]+_ip[8]=="OOO")){
+                            if(wincount==0){win(player1Name);}
+                        }
+                        else if((_ip[0]+_ip[3]+_ip[6]=="OOO")||(_ip[1]+_ip[4]+_ip[7]=="OOO")||(_ip[2]+_ip[5]+_ip[8]=="OOO")){
+                            if(wincount==0){win(player1Name);}
+                            
+                        }
+                        else if((_ip[0]+_ip[4]+_ip[8]=="OOO")||(_ip[2]+_ip[4]+_ip[6]=="OOO")){
+                            if(wincount==0){win(player1Name);}
+                            
+                        }
+                    }
+                    
+                    else if((gridbox[i].count==0&&turncount%2!=0)&&(cpuValue==2||player2Value==2)){
+                        gridbox[i].innerHTML="X";
+                        _ip[gridbox[i].id]=_ip[parseInt(gridbox[i].id)]+"X";
+                        turncount=turncount+1;
+                        gridbox[i].count=1;
+                        
+                        if((_ip[0]+_ip[1]+_ip[2]=="XXX")||(_ip[3]+_ip[4]+_ip[5]=="XXX")||(_ip[6]+_ip[7]+_ip[8]=="XXX")){
+                            if(wincount==0){win(player1Name);}
+                        }
+                        else if((_ip[0]+_ip[3]+_ip[6]=="XXX")||(_ip[1]+_ip[4]+_ip[7]=="XXX")||(_ip[2]+_ip[5]+_ip[8]=="XXX")){
+                            if(wincount==0){win(player1Name);}
+                        }
+                        else if((_ip[0]+_ip[4]+_ip[8]=="XXX")||(_ip[2]+_ip[4]+_ip[6]=="XXX")){
+                            if(wincount==0){win(player1Name);}
+                        }
+                    }
+               
+            });
+    
+            }
+        
+    }
+    function win(winner){
+        document.getElementById("message").innerHTML="Congratulations !!<br> <span style='font-size:40px;'>"+winner+"</span>"+"<br>wins the match";
+        wincount=1;
+    }
+    creategrid();
 }
-creategrid();
+game();
